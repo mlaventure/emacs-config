10c401fcca861e44add7d04eca4ab5ea23869f72
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2019-02-13 09:47:28 -0800

    Add Rust config
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,3 +11,20 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:update-interval 2)
+ '(package-selected-packages
+   (quote
+    (racer flycheck-rust rust-mode vlf helm-projectile projectile markdown-mode yaml-mode protobuf-mode toml-mode dockerfile-mode git-gutter-fringe direnv go-playground godoctor go-guru company-go go-eldoc go-mode company flycheck helm-flx flx helm-fuzzier helm undo-tree column-enforce-mode fill-column-indicator spaceline-all-the-icons moe-theme spaceline fancy-battery highlight-thing smartparens which-key neotree buffer-move nlinum default-text-scale all-the-icons rainbow-delimiters diminish auto-compile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(hl-line ((t (:background "steel blue"))))
+ '(idle-highlight ((t (:inherit region :background "tomato")))))

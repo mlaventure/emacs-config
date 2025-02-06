aefd1b750684db075c607a9cffb0efd06a6a3638
Kenfe-Mickael Laventure <mickael@impulselabs.com>
2025-02-06 09:59:05 -0800

    ellama: use models/settings from official README
    Signed-off-by: Kenfe-Mickael Laventure <mickael@impulselabs.com>

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -240,31 +240,54 @@
   (setopt ellama-language "English")
   (require 'llm-ollama)
   (setopt ellama-provider
-	  (make-llm-ollama
-	   ;; this model should be pulled to use it
-	   ;; value should be the same as you print in terminal during pull
-	   :chat-model "llama3.1"
-	   :embedding-model "nomic-embed-text"
-	   :default-chat-non-standard-params '(("num_ctx" . 1024))))
+          (make-llm-ollama
+           ;; this model should be pulled to use it
+           ;; value should be the same as you print in terminal during pull
+           :chat-model "llama3:8b-instruct-q8_0"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  (setopt ellama-summarization-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5:3b"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  (setopt ellama-coding-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5-coder:3b"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  ;; Predefined llm providers for interactive switching.
+  ;; You shouldn't add ollama providers here - it can be selected interactively
+  ;; without it. It is just example.
   (setopt ellama-providers
-	  '(("zephyr" . (make-llm-ollama
-			 :chat-model "zephyr"
-			 :embedding-model "zephyr"))
-
-	    ("llama3.1" . (make-llm-ollama
-			   :chat-model "llama3.1"
-			   :embedding-model "llama3.1"))
-	    ("mixtral" . (make-llm-ollama
-			  :chat-model "mixtral"
-			  :embedding-model "mixtral"))))
+          '(("zephyr" . (make-llm-ollama
+                         :chat-model "zephyr:7b-beta-q6_K"
+                         :embedding-model "zephyr:7b-beta-q6_K"))
+            ("mistral" . (make-llm-ollama
+                          :chat-model "mistral:7b-instruct-v0.2-q6_K"
+                          :embedding-model "mistral:7b-instruct-v0.2-q6_K"))
+            ("mixtral" . (make-llm-ollama
+                          :chat-model "mixtral:8x7b-instruct-v0.1-q3_K_M"
+                          :embedding-model "mixtral:8x7b-instruct-v0.1-q3_K_M"))))
+  ;; Naming new sessions with llm
+  (setopt ellama-naming-provider
+          (make-llm-ollama
+           :chat-model "llama3:8b-instruct-q8_0"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("stop" . ("\n")))))
   (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
   ;; Translation llm provider
-  (setopt ellama-translation-provider (make-llm-ollama
-				       :chat-model "mixtral"
-				       :embedding-model "nomic-embed-text"))
-  (setq llm-warn-on-nonfree nil)
-  (setq ellama-sessions-directory "~/.config/emacs/.local/cache/ellama-sessions/"
-        ellama-sessions-auto-save t))
+  (setopt ellama-translation-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5:3b"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params
+           '(("num_ctx" . 32768))))
+  (setopt ellama-extraction-provider (make-llm-ollama
+                                      :chat-model "qwen2.5-coder:7b-instruct-q8_0"
+                                      :embedding-model "nomic-embed-text"
+                                      :default-chat-non-standard-params
+                                      '(("num_ctx" . 32768)))))
 
 (map! :leader
       (:prefix-map ("l" . "ellama")

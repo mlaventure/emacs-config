43b4cc25f7e48e80c8bac166ff2c9241ac08e2e9
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2025-02-16 12:48:06 -0800

    Swich to venice.ai model
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -236,64 +236,31 @@
 (setq gc-cons-threshold (* 1024 1024 1014)) ;; 100MiB
 (setq read-process-output-max (* 8 1024 1024)) ;; 3MiB
 
+(defun venice-api-key ()
+  (with-temp-buffer
+    (insert-file-contents "~/.venice.key")
+    (buffer-substring-no-properties (point-min) (point-max))))
+
 (after! ellama
   (setopt ellama-language "English")
   (require 'llm-ollama)
+  (require 'llm-openai)
+  (setq llm-warn-on-nonfree nil)
   (setopt ellama-provider
-          (make-llm-ollama
-           ;; this model should be pulled to use it
-           ;; value should be the same as you print in terminal during pull
-           :chat-model "llama3:8b-instruct-q8_0"
+          (make-llm-openai-compatible
+           :url "https://api.venice.ai/api/v1"
+           :key (venice-api-key)
+           :chat-model "llama-3.3-70b"
            :embedding-model "nomic-embed-text"
-           :default-chat-non-standard-params '(("num_ctx" . 8192))))
-  (setopt ellama-summarization-provider
-          (make-llm-ollama
-           :chat-model "qwen2.5:3b"
-           :embedding-model "nomic-embed-text"
-           :default-chat-non-standard-params '(("num_ctx" . 32768))))
-  (setopt ellama-coding-provider
-          (make-llm-ollama
-           :chat-model "qwen2.5-coder:3b"
-           :embedding-model "nomic-embed-text"
-           :default-chat-non-standard-params '(("num_ctx" . 32768))))
-  ;; Predefined llm providers for interactive switching.
-  ;; You shouldn't add ollama providers here - it can be selected interactively
-  ;; without it. It is just example.
-  (setopt ellama-providers
-          '(("zephyr" . (make-llm-ollama
-                         :chat-model "zephyr:7b-beta-q6_K"
-                         :embedding-model "zephyr:7b-beta-q6_K"))
-            ("mistral" . (make-llm-ollama
-                          :chat-model "mistral:7b-instruct-v0.2-q6_K"
-                          :embedding-model "mistral:7b-instruct-v0.2-q6_K"))
-            ("mixtral" . (make-llm-ollama
-                          :chat-model "mixtral:8x7b-instruct-v0.1-q3_K_M"
-                          :embedding-model "mixtral:8x7b-instruct-v0.1-q3_K_M"))))
-  ;; Naming new sessions with llm
-  (setopt ellama-naming-provider
-          (make-llm-ollama
-           :chat-model "llama3:8b-instruct-q8_0"
-           :embedding-model "nomic-embed-text"
-           :default-chat-non-standard-params '(("stop" . ("\n")))))
-  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
-  ;; Translation llm provider
-  (setopt ellama-translation-provider
-          (make-llm-ollama
-           :chat-model "qwen2.5:3b"
-           :embedding-model "nomic-embed-text"
-           :default-chat-non-standard-params
-           '(("num_ctx" . 32768))))
-  (setopt ellama-extraction-provider (make-llm-ollama
-                                      :chat-model "qwen2.5-coder:7b-instruct-q8_0"
-                                      :embedding-model "nomic-embed-text"
-                                      :default-chat-non-standard-params
-                                      '(("num_ctx" . 32768)))))
+           )))
 
 (require 'transient)
 (require 'ellama)
 
 (map! :leader
       (:prefix-map ("l" . "ellama")
+       :desc "Chat"             "C"     #'ellama-chat
+       :desc "Send last msg"    "L"     #'ellama-chat-send-last-message
        :desc "Ask"              "a"     #'ellama-transient-ask-menu
        :desc "Code"             "c"     #'ellama-transient-code-menu
        :desc "Improve"          "i"     #'ellama-transient-improve-menu
